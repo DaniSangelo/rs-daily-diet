8d35c0bd251721f899dcdd4c2ec528e85800d0f4
Daniel Sângelo <danielsangelo@hotmail.com>
2024-08-24 20:01:42 -0300

    Update meal

--- src/routes/mealRoutes.ts
+++ src/routes/mealRoutes.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 import { setUser } from "../middlewares/setUser";
 import { knex } from "../database";
 import { randomUUID } from "crypto";
+import { describe } from "node:test";
+import { format } from "date-fns";
 
 export async function mealRoutes(app: FastifyInstance) {
     app.post('/', 
@@ -23,5 +25,28 @@ export async function mealRoutes(app: FastifyInstance) {
             ...meal
         })
         res.status(201).send()
+    })
+
+    app.put('/:id',
+        {
+            preHandler: [setUser]
+        },
+        async (req, res) => {
+        const updateMealSchema = z.object({
+            name: z.string(),
+            description: z.string(),
+            mealDate: z.string(),
+            isOnDiet: z.boolean()
+        })
+        const paramsSchema = z.object({ id: z.string().uuid() })
+        const { id } = paramsSchema.parse(req.params)
+        const mealData = updateMealSchema.parse(req.body)
+        const meal = await knex('meal').where({ id }).select('id').first()
+        if (!meal) res.status(404).send({ message: 'Meal not found '})
+        await knex('meal').update({
+            updated_at: format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
+            ...mealData,
+        })
+        res.status(204).send()
     })
 }
